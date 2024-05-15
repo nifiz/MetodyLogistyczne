05b1f72e36f19a56211658faf82995a5a4892521
Adrian Janowski <janowskiadrian02@gmail.com>
2024-05-15 12:02:52 +0200

    initial

--- Projekcior/main.py
+++ Projekcior/main.py
@@ -2,8 +2,4 @@ import MainWindow
 
 if __name__ == "__main__":
     app = MainWindow.App()
-    app.title("Logistyka")
-    app.configure(background="white")
-    app.minsize(700, 600)
-    app.maxsize(1024, 800)
     app.mainloop()
