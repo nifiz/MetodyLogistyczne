48d19a21d5f55277cb0a09cb7922a66296ad5c19
0bKP <kacper.pajor42@gmail.com>
2024-05-19 13:55:41 +0200

    okno glowne

--- Projekcior/MainWindow.py
+++ Projekcior/MainWindow.py
@@ -1,15 +1,14 @@
 import tkinter as tk
-from PIL import Image, ImageTk
 
 frameHeight = 100
 menuWidth = 135
 logoWidth = 340
-exitWidth = 300
-#tabsWidth = 1920-menuWidth-logoWidth-exitWidth
+exitWidth = 220
 buttonWidth = 326
 tabsWidth = 3*buttonWidth
 bgColour = "#141218"
 borderColour = "#6750A4"
+fontColour = "#CAC4D0"
 
 class App(tk.Tk):
     def __init__(self):
@@ -23,15 +22,16 @@ class App(tk.Tk):
 
         toolbar = tk.Frame(self, bg=bgColour, height=frameHeight, highlightbackground=borderColour, highlightthickness=1)
         toolbar.pack(side="top", fill="x")
-        toolbarMenu = tk.Frame(toolbar, bg=bgColour, width=menuWidth, height=frameHeight, highlightbackground=borderColour, highlightthickness=1)
-        toolbarLogo = tk.Frame(toolbar, bg=bgColour, width=logoWidth, height=frameHeight, highlightbackground=borderColour, highlightthickness=1)
-        toolbarTabs = tk.Frame(toolbar, bg=bgColour, width=tabsWidth, height=frameHeight, highlightbackground=borderColour, highlightthickness=1)
-        toolbarExit = tk.Frame(toolbar, bg=bgColour, width=exitWidth, height=frameHeight, highlightbackground=borderColour, highlightthickness=1)
+        toolbarMenu = tk.Frame(toolbar, bg=bgColour, width=menuWidth, height=frameHeight, highlightbackground=borderColour)
+        toolbarLogo = tk.Frame(toolbar, bg=bgColour, width=logoWidth, height=frameHeight, highlightbackground=borderColour)
+        toolbarTabs = tk.Frame(toolbar, bg=bgColour, width=tabsWidth, height=frameHeight, highlightbackground=borderColour, highlightthickness=0)
+        toolbarExit = tk.Frame(toolbar, bg=bgColour, width=exitWidth, height=frameHeight, highlightbackground=borderColour, highlightthickness=0)
 
         toolbarMenu.pack(side="left")
         toolbarLogo.pack(side="left")
         toolbarTabs.pack(side="left")
         toolbarExit.pack(side="right")
+        toolbarExit.propagate(False)
 
         # Obrazki
         self.menu_image = tk.PhotoImage(file=r"img\burger.png")
@@ -40,33 +40,39 @@ class App(tk.Tk):
         self.load_data = tk.PhotoImage(file=r"img\load_data.png")
 
         # Przyciski
-        menu_button = tk.Button(toolbarMenu, image=self.menu_image, fg="#FFFFFF", bg=bgColour, height=10, width=18, borderwidth=0, highlightthickness=0)
-        logo_button = tk.Button(toolbarLogo, height=5, text="Logo", fg="#FFFFFF", bg=bgColour, width=60, borderwidth=0, highlightthickness=0)
-        analButton = tk.Button(toolbarTabs, height=5, text="Analiza", fg="#FFFFFF", bg=bgColour, width=43, borderwidth=0, highlightthickness=0)
-        mapButton = tk.Button(toolbarTabs, height=5, text="Mapa", fg="#FFFFFF", bg=bgColour, width=43, borderwidth=0, highlightthickness=0)
-        dataButton = tk.Button(toolbarTabs, height=5, text="Dane", fg="#FFFFFF", bg=bgColour, width=43, borderwidth=0, highlightthickness=0)
-        exit_button = tk.Button(toolbarExit, image=self.exit_button, height=5, fg="#FFFFFF", bg=bgColour, width=25, borderwidth=0, highlightthickness=0)
+        menu_button = tk.Button(toolbarMenu, image=self.menu_image, bg=bgColour, height=15, width=18,
+                                highlightthickness=0, borderwidth=0)
+        logo_button = tk.Button(toolbarLogo, height=5, text="Paliwska", fg="white", bg=bgColour,
+                                width=40, borderwidth=0, highlightthickness=0, font="Roboto 12 bold")
+        analButton = tk.Button(toolbarTabs, height=5, text="Analiza", fg=fontColour, bg=bgColour, width=33,
+                               borderwidth=0, highlightthickness=0, font="Roboto")
+        mapButton = tk.Button(toolbarTabs, height=5, text="Mapa", fg=fontColour, bg=bgColour, width=33, borderwidth=0,
+                              highlightthickness=0, font="Roboto")
+        dataButton = tk.Button(toolbarTabs, height=5, text="Dane", fg=fontColour, bg=bgColour, width=33, borderwidth=0,
+                               highlightthickness=0, font="Roboto")
+        exit_button = tk.Button(toolbarExit, image=self.exit_button, height=5, bg=bgColour, width=15,
+                                borderwidth=0, highlightthickness=0, padx=0, pady=0)
 
 
-        menu_button.pack(expand=True, fill="both")
+        menu_button.pack(expand=True, fill="both", padx=15)
         logo_button.pack(expand=True, fill="both")
-        analButton.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-        mapButton.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-        dataButton.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-        exit_button.pack(expand=True, fill=tk.BOTH)
+        analButton.pack(side=tk.LEFT, expand=True, fill="both")
+        mapButton.pack(side=tk.LEFT, expand=True, fill="both")
+        dataButton.pack(side=tk.LEFT, expand=True, fill="both")
+        exit_button.pack(side=tk.RIGHT, expand=True, fill="both")
 
         menu_button.config(width=41, height=82)
         exit_button.config(width=352, height=82)
 
-        mainScreen = tk.Frame(self, bg=bgColour, height=frameHeight, highlightbackground=borderColour, highlightthickness=1)
-        mainScreen.pack(expand=True, fill="both")
-        empty_screen = tk.Canvas(mainScreen, bg=bgColour, width=660, height=432, highlightthickness=0)
-        empty_screen.create_image(660, 432, image=self.empty_state_image, anchor="w")
-        empty_screen.pack(expand=True, fill="both")
-        load_data = tk.Button(mainScreen, image=self.load_data, bg=bgColour, highlightthickness=0, borderwidth=0)
-        load_data.pack(side="top", fill='y')
 
+        self.main_frame = tk.Frame(self, bg=bgColour)
+        self.main_frame.pack(expand=True, fill=tk.BOTH)
 
+        self.image_label = tk.Label(self.main_frame, image=self.empty_state_image, bg=bgColour)
+        self.image_label.pack(pady=(100, 0))
+
+        self.main_button = tk.Button(self.main_frame, image=self.load_data, bg=bgColour, borderwidth=0, highlightthickness=0)
+        self.main_button.pack(pady=30)
 
 
     def say_hello(self):
